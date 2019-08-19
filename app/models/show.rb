f8501b77f051424061443a28ccdae7ed88d89755
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-19 16:19:16 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-  self.where("rating = ?",self.highest_rating)
+  self.where("rating = ?",self.highest_rating).first
  
   end  
   
@@ -15,7 +15,7 @@ class Show < ActiveRecord::Base
   end  
   
   def self.least_popular_show
-    self.where("rating = ?",self.lowest_rating)
+    self.where("rating = ?",self.lowest_rating).last
  
   end
   
